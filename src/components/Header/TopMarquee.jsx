de8e86458988951a2ecfa6be08c8ad10d063b373
Ankit-Coder-latehar <chotuchaudhary300@gmail.com>
2026-08-14 18:28:58 +0530

    my latest changes

--- src/components/Header/TopMarquee.jsx
+++ src/components/Header/TopMarquee.jsx
@@ -8,7 +8,7 @@ export default function TopMarquee() {
           <span className="text-[#c0392b] font-bold">WELCOME TO KCNAVKAR.COM</span>
           <span className="text-black mx-1">:</span>
           <span className="text-[#666600]"> CASH ON DELIVERY WITH EXPRESS COURIER SERVICE AND DOMESTIC AND INTERNATIONAL SHIPPING </span>
-          <span className="text-[#e67e22] font-bold">(POWERED BY ART RIDDH MANUFACTURER BRAND)</span>
+          <span className="text-[#e67e22] font-bold">(POWERED BY Maa Sankhwali creation)</span>
           <span className="mx-8 text-gray-400">|</span>
           <span className="text-[#c0392b] font-bold">WELCOME TO KCNAVKAR.COM</span>
           <span className="text-black mx-1">:</span>
